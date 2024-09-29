fe8d3928d34cc9bd59f25452c400f635605b7fff
Satyam-Nayak <satyamn120@gmail.com>
2024-09-29 12:57:49 +0530

    finall

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
+import React from 'react';
+
 import Intro from './Intro';
 import Compo1 from './Compo1';
 import Compo2 from './Compo2';
 import Compo3 from './Compo3';
+
 
 function App() {
   console.log("Welcome to my folder");
