8030e0411835b1d0cc2025a2707827477a63a702
Satyam-Nayak <satyamn120@gmail.com>
2024-10-14 18:46:40 +0530

    Compo4 defination added

--- src/Compo4.jsx
+++ src/Compo4.jsx
@@ -13,10 +13,11 @@ import React from 'react'
         <div>
         <h2 >"npm" : </h2>
         <ul>
-            <li>npm stands for Node Package Manager.</li>
+            <li>npm stands for (Node Package Manager).</li>
             <li>npm is a collection of JavaScript libraries and resources that can be utilized in any JavaScript application.</li>
             <li>npm resources can be downloaded with the help of the Node.js application.</li>
             <li>Node.js is a server-side application that allows the user to run JavaScript programs on any system.</li>
+            <li>Node.js is a runtime environment that allows you to execute JavaScript on the server-side (outside of a browser). It provides a command-line interface (CLI) to perform backend operations using JavaScript.</li>
         </ul>
         </div>
     </React.Fragment>
